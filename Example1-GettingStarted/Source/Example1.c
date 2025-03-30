0086eae2e44aa214641dca0c79c13031cdcd5f25
Jonathan Fischer <jonathan@mohiji.org>
2025-03-29 19:44:41 -0700

    Finishing touches on Example 1, README with building instructions.

--- Example1-GettingStarted/Source/Example1.c
+++ Example1-GettingStarted/Source/Example1.c
@@ -11,59 +11,62 @@
 // Pull in SDL3, obviously.
 #include <SDL3/SDL.h>
 
-// And only include SDL_main.h in the file where you define your main function(s).
+// Include SDL_main.h in the file where you define your main function(s).
 #include <SDL3/SDL_main.h>
 
-// Our application context structure. For now, we have our
-// window and GPU device, but this'll grow in later posts.
+// We'll have some things we want to keep track of as we move through the lifecycle functions.
+// Globals would be fine for this example, but SDL gives you a way to pipe a data structure
+// through the functions too, so we'll use that.
+//
+// For now, we need to keep track of the window we're creating and the GPU driver device.
 typedef struct AppContext {
     SDL_Window* window;
     SDL_GPUDevice* device;
 } AppContext;
 
-// SDL_AppInit is the first function that will be called.
-// This is where you initialize SDL, load resources that
-// your game will need from the start, etc.
-SDL_AppResult SDL_AppInit(void** appState, int argc, char** argv)
+// SDL_AppInit is the first function that will be called. This is where you initialize SDL,
+// load resources that your game will need from the start, etc.
+SDL_AppResult SDL_AppInit(
+    // Allows you to return a data structure to pass through
+    void** appState,
+
+    // Normal main argc & argv
+    int argc, char** argv)
 {
-    // This isn't strictly necessary, but if you provide
-    // a little bit of metadata here SDL will use it in
-    // things like the About window on macOS.
+    // This isn't strictly necessary, but if you provide a little bit of metadata here SDL
+    // will use it in things like the About window on macOS.
     SDL_SetAppMetadata("GPU by Example - Getting Started", "0.0.1", "net.jonathanfischer.GpuByExample1");
 
     // Initialize the video and event subsystems
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-            "Couldn't initialize SDL: %s", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    // This surprised me, but you create the GPU device first, then the window.
-    // You'll need to tell the API up front what shader languages you plan on
-    // supporting.
-    SDL_GPUShaderFormat shaderFormats = SDL_GPU_SHADERFORMAT_SPIRV |
-                                        SDL_GPU_SHADERFORMAT_DXIL |
-                                        SDL_GPU_SHADERFORMAT_MSL;
-
-    SDL_GPUDevice* device = SDL_CreateGPUDevice(shaderFormats, false, NULL);
-    if (device == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-            "Couldn't not create GPU device: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
-
-    // Now go ahead and create a window. I'm creating a high pixel density
-    // window because without that, I was getting blurry text on macOS. (text
-    // comes in a later post, promise.)
-    SDL_WindowFlags windowFlags =
-        SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_RESIZABLE;
-    SDL_Window* window =
-        SDL_CreateWindow("GPU by Example - Scaffolding", 800, 600, windowFlags);
+    // Create a window. I'm creating a high pixel density window because without that, I was getting
+    // blurry text on macOS. (text comes in a later post, promise.)
+    SDL_WindowFlags windowFlags = SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_RESIZABLE;
+    SDL_Window* window = SDL_CreateWindow("GPU by Example - Getting Started", 800, 600, windowFlags);
 
     if (window == NULL) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
+    // Next up, let's create a GPU device. You'll need to tell the API up front what shader languages
+    // you plan on supporting. SDL looks through its list of drivers in "a reasonable order" to pick
+    // which one to use. Fun surprise here: on Windows, it's going to prefer Vulkan over Direct3D 12
+    // if it's available. Here, we're enabling Vulkan (SPIRV), Direct3D 12 (DXIL), and Metal (MSL).
+    SDL_GPUShaderFormat shaderFormats = SDL_GPU_SHADERFORMAT_SPIRV | SDL_GPU_SHADERFORMAT_DXIL | SDL_GPU_SHADERFORMAT_MSL;
+
+    SDL_GPUDevice* device = SDL_CreateGPUDevice(shaderFormats, false, NULL);
+    if (device == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't not create GPU device: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Just so we know what we're working with, log the driver that SDL picked for us.
+    SDL_Log("Using %s GPU implementation.", SDL_GetGPUDeviceDriver(device));
 
     // Then bind the window and GPU device together
     if (!SDL_ClaimWindowForGPUDevice(device, window)) {
@@ -71,14 +74,12 @@ SDL_AppResult SDL_AppInit(void** appState, int argc, char** argv)
         return SDL_APP_FAILURE;
     }
 
-    // By default, SDL GPU enables VSYNC, which is generally what I want. If you
-    // want to change it, now is the time to do that; look at
-    // SDL_SetGPUSwapchainParameters in the documentation.
+    // By default, SDL GPU enables VSYNC, which is generally what I want. If you want to change it, now is
+    // the time to do that; look at SDL_SetGPUSwapchainParameters in the documentation.
     // https://wiki.libsdl.org/SDL3/SDL_SetGPUSwapchainParameters
 
-    // Last up, let's create our context object and store pointers to our window
-    // and GPU device. We stick it in the appState argument passed to this
-    // function and SDL will provide it in later calls.
+    // Last up, let's create our context object and store pointers to our window and GPU device. We stick it
+    // in the appState argument passed to this function and SDL will provide it in later calls.
     AppContext* context = SDL_malloc(sizeof(AppContext));
     context->window = window;
     context->device = device;
@@ -105,6 +106,8 @@ SDL_AppResult SDL_AppIterate(void* appState)
         return SDL_APP_FAILURE;
     }
 
+    // As I understand it, _this_ is where it's going to wait for Vsync, not in the loop
+    // that calls SDL_AppIterate.
     SDL_GPUTexture* swapchainTexture;
     if (!SDL_WaitAndAcquireGPUSwapchainTexture(cmdBuf, context->window, &swapchainTexture, NULL, NULL)) {
         SDL_Log("SDL_WaitAndAcquireGPUSwapchainTexture: %s", SDL_GetError());
@@ -140,8 +143,8 @@ SDL_AppResult SDL_AppIterate(void* appState)
         SDL_EndGPURenderPass(renderPass);
     }
 
-    // And finally, submit the command buffer for drawing. The driver will take over at this point
-    // and do all the rendering we've asked it to.
+    // And finally, submit the command buffer for drawing. The driver will take over at this point and do
+    // all the rendering we've asked it to.
     SDL_SubmitGPUCommandBuffer(cmdBuf);
 
     // That's it for this frame.
@@ -150,9 +153,16 @@ SDL_AppResult SDL_AppIterate(void* appState)
 
 SDL_AppResult SDL_AppEvent(void* appState, SDL_Event* event)
 {
+    // SDL_EVENT_QUIT is sent when the main (last?) application window closes.
     if (event->type == SDL_EVENT_QUIT) {
         // SDL_APP_SUCCESS means we're making a clean exit.
         // SDL_APP_FAILURE would mean something went wrong.
+        return SDL_APP_SUCCESS;
+    }
+
+    // For convenience, I'm also quitting when the user presses the escape
+    // key. It makes life easier when I'm testing on a Steam Deck.
+    if (event->type == SDL_EVENT_KEY_DOWN && event->key.key == SDLK_ESCAPE) {
         return SDL_APP_SUCCESS;
     }
 
@@ -164,6 +174,8 @@ void SDL_AppQuit(void* appState, SDL_AppResult result)
 {
     AppContext* context = (AppContext*)appState;
 
+    // Just cleaning things up, making sure we're working with valid pointers
+    // as we go.
     if (context != NULL) {
         if (context->device != NULL) {
             if (context->window != NULL) {
--- README.md
+++ README.md
@@ -4,3 +4,73 @@ This repository goes along with the "Muddling through SDL GPU" set of posts
 at https://www.jonathanfischer.net/. It's basically me, someone who never
 moved beyond OpenGL 1.2, learning how to use a modern GPU API, and shaders,
 and all that fun stuff.
+
+## Building and Running examples
+
+I've tried to make this as easy as I can: the necessary libraries are included in this
+repository, and everything should be linked up and ready to run in the normal dev tools
+for each platform.
+
+### macOS
+
+You'll probably need to remove the quarantine xattr from the repository before Xcode will
+work without complaining at you. To do that, pop open terminal and run
+
+    xattr -r -d com.apple.quarantine /path/to/repository
+
+Open `GpuByExample.xcworkspace` in Xcode. Each example is included as a separate project:
+you can run them by selecting the example you want to run in the Build Scheme dropdown
+and running it. I've left code signing turned off, SDL's included as an xcframework, and
+the necessary resources get copied into each example's app bundle, so it should just
+work.
+
+![Xcode](/Images/xcode.png "Build Scheme dropdown in Xcode 16.1")
+
+### Windows
+
+Open `GpuByExample.sln` in Visual Studio. Each example is included as a separate project:
+you can run then by right clicking the example in the Solution Explorer and selecting
+"Set as Startup Project", then run or debug it. The necessary SDL libraries for x86 and
+x86_64 are included in the Libraries directory in the repository, and each example should
+be configured to both link the appropriate .lib files and find the appropriate .dll files
+at runtime. Each example should also be configured to run with its working directory as
+the appropriate resources directory, so it'll find any necessary shaders or other resources
+at runtime.
+
+> Quick caveat: I can't verify that this works at the moment. The computer I
+> use to build the examples is 13 years old and has integrated graphics; it won't run any
+> of this. I borrow a little time to test these out on my son's gaming PC, but I don't
+> have Visual Studio installed up there.
+
+![Visual Studio](/Images/visual-studio.png "Set as Startup Project in Visual Studio 2022")
+
+### Linux
+
+This is more manual, sorry: I haven't taken time to make this automatic yet. First, SDL
+is included as a submodule of this repository, so you'll need to make sure you're pulling
+that down as well. Either clone it with `git clone --recursive` in the first place, or
+after cloning it run `git submodule init` followed by `git submodule update` to clone
+SDL.
+
+To build and run an example, open up your terminal and switch to the example's
+directory. Run:
+
+    cmake -B build/
+
+to generate the necessary build files for the example, then
+
+    cmake --build build/
+
+to actually build the example. Each example will build its own copy of SDL, sorry, I'm not
+sure how to avoid that yet. Copy the contents of the example's Resources directory into
+the build/ directory, then switch into that directory and run the example: it'll be named
+`gbe-example<something>`. You'll need to make sure it can find the SDL library with 
+`LD_LIBRARY_PATH`. So the full set of steps for Example 2 would look like:
+
+    cd /path/to/repo/Example2-DrawingPrimitives
+    cmake -B build/
+    cmake --build build/
+    cd build/
+    cp -r ../Resources/* ./
+    LD_LIBRARY_PATH=`pwd` ./gbe-example2-drawing-primitives
+
